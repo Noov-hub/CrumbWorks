cb1fabbcb60fc55a528d8d77534be782213821bc
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-02-26 12:24:13 +0700

    Update if-else.py

--- if-else.py
+++ if-else.py
@@ -1,10 +1,12 @@
+# Date Of Creation 8/13/2022
+
 print('2 + 3 = ')
 jawaban = int(input('jawaban lu : '))
 
 if (jawaban == 5):
     print('nahh')
 else:
-    print('BLOG!')
+    print('WTF bro!')
     jawaban = int(input('coba lgi sono:'))
     if (jawaban == 5):
         print('nahh gitu dongg..')
