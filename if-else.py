92df40997d18998b4f15a1c136587413a5d53d43
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-02-26 12:22:54 +0700

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,12 @@
+print('2 + 3 = ')
+jawaban = int(input('jawaban lu : '))
+
+if (jawaban == 5):
+    print('nahh')
+else:
+    print('BLOG!')
+    jawaban = int(input('coba lgi sono:'))
+    if (jawaban == 5):
+        print('nahh gitu dongg..')
+    else:
+        print('dahlah...males gw asw')
