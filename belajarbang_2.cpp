95a0fc7ec96c25fa460e2fd4af1a17d27192dd5a
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-03-06 00:00:51 +0700

    Add files via upload

---
+++ belajarbang_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+#define P 16
+#define L 3
+
+int main()
+{
+    const string NAMA = "kirtak";
+    const double TINGGI = 170.4;
+    
+    cout << "nama = " << NAMA;
+    cout << "\n";
+  
+    cout << "tingginya = " << TINGGI;
+    cout << "\n";
+    
+    cout << "panjangnya = " << P;
+    cout << "\n";
+
+    cout << "lebarnya = " << L;
+    cout << "\n";
+
+    cout << "luasnya = " << P*L;
+    cout << "\n";
+    
+    cout << "panjang apaan tuhh?";
+    
+    return 0;
+}
