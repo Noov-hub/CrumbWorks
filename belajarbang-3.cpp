2bf44d8fcfec74947975a9d2de8cf3bd48d75620
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-03-06 23:24:34 +0700

    Add files via upload

---
+++ belajarbang-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#
+using namespace std;
+
+int main()
+{
+    int harga;
+    
+    cout << "masukin angka BLOKK!!: ";
+    cin >> harga;
+    
+    cout << "HARGA DIRI LO: " << "Rp." << (harga - 10000) << endl;
+    cout << "*kena diskon ama pemerintah";
+    
+    return 0;
+}
