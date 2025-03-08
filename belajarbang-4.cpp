8f8ef2d7b2c860984d0ef8eaec859a100c4c6025
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-03-08 22:36:20 +0700

    Add files via upload

---
+++ belajarbang-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    int umur;
+    
+    cout << "masukan nama mu!: ";
+    getline(cin,nama);
+    
+    cout << "masukan umur mu! :";
+    cin >> umur;
+    
+    cout << "\n";
+    
+    cout << "selamat datang " << nama << ",semangat!!" << endl;
+    cout << "usia lu sekarang " << umur << " tahun," << "besok lu udah mau kuliah..." <<endl;
+    
+    return 0;
+}
