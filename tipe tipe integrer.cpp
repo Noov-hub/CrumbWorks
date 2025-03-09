f0ffe7347c6f43cb4e59b1792eb5c55530004d84
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-03-09 21:27:48 +0700

    Add files via upload

---
+++ tipe tipe integrer.cpp
@@ -0,0 +1,21 @@
+//tipe data integer
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    short angka1;
+    int angka2;
+    long angka3;
+
+    angka1 = 12354;
+    angka2 = 123443135;
+    angka3 = 325235325;
+
+    cout << "isi variabel angka1 (short) " >> angka1 << endl;
+    cout << "isi variabel angka2 (int) " >> angka2 << endl;
+    cout << "isi variabel angka3 (long) " >> angka3 << endl;
+
+    return 0;
+}
